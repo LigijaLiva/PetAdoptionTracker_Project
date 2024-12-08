9d9d9867bc5186086e212f5bb9df3e66955334e1
LigijaLiva <ligijaliva@gmail.com>
2024-12-08 22:27:17 +0200

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module nosaukums {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires java.desktop;
+	requires java.sql;
+	requires javafx.base;
+	
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
+}
