bdfa5961bac47a558bcc57038d71c4279964accf
LigijaLiva <ligijaliva@gmail.com>
2024-12-09 01:31:40 +0200

    Update MainApp.java
    Updated the class

--- MainApp.java
+++ MainApp.java
@@ -7,10 +7,18 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Izveido DatabaseMigrator objektu un migrē datu bāzi
+        DatabaseMigrator migrator = new DatabaseMigrator();
+        migrator.migrateDatabase();  // Migrācijas izpilde
+
+        // Attēla parādīšana
         ImageView imageView = new ImageView();
         Image image = new Image("/images/cat2.jpg");
         imageView.setImage(image);
@@ -21,7 +29,6 @@ public class MainApp extends Application {
         Scene scene = new Scene(root, 400, 300);
 
         primaryStage.getIcons().add(image);
-
         primaryStage.setTitle("Pet Adoption Tracker");
         primaryStage.setScene(scene);
         primaryStage.show();
